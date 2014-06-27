127fe7ae10f64b487b84b35c918ccab64b0b6cc7
Sean Kelly <kelly@seankelly.biz>
2014-06-27 09:38:57 -0500

    fix copyright year

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-# Copyright 2009-2010 California Institute of Technology. ALL RIGHTS
+# Copyright 2009-2014 California Institute of Technology. ALL RIGHTS
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
 from setuptools import setup, find_packages
