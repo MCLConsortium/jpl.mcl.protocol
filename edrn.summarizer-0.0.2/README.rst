a6f2bee255c5dbf46849ca494645ecda1452a2e8
yuliu <yuliu@bioinftest.attlocal.net>
2016-04-06 23:15:08 -0700

    removed unnessary packaging of this module

--- edrn.summarizer-0.0.2/README.rst
+++
@@ -1,3 +0,0 @@
-Server for the Early Detection Research Network (EDRN) that provides
-summary of EDRN's knowledge using the various summarization methods.
-
--- edrn.summarizer-0.0.2/edrn/summarizer/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-# Copyright 2008 California Institute of Technology. ALL RIGHTS
-# RESERVED. U.S. Government Sponsorship acknowledged.
-
-'''
-EDRN RDF Service: unit and functional tests.
-'''
